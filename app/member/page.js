ca2cdb72b3a3717b938955ec30ad6d75471b9695
MikaelaMathisson <128062820+MikaelaMathisson@users.noreply.github.com>
2024-10-19 00:18:56 +0200

    changes to member and members pages

--- app/member/page.js
+++ app/member/page.js
@@ -1,5 +1,7 @@
+// app/member/page.js
 "use client";
 import React, { useState } from "react";
+import "../globals.css"; // Corrected import path
 
 const Page = () => {
   const [formData, setFormData] = useState({
@@ -24,183 +26,211 @@ const Page = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Hantera formulärinlämning här
     console.log("Form data submitted:", formData);
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-50 p-5 font-sans">
-      <div className="bg-white shadow-lg rounded-lg p-8 max-w-2xl w-full">
-        <h1 className="text-3xl font-bold mb-4">Bli medlem</h1>
-        <p className="text-lg mb-4">
-          Skicka ett meddelande till oss idag med hjälp av formuläret eller
-          kontakta oss via informationen nedan. Vi hjälper dig gärna med dina
-          frågor. Vi kommer att svara så snart som möjligt.
-        </p>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-bold mb-2"
-              htmlFor="membershipType"
-            >
-              Typ av medlemskap
-            </label>
-            <select
-              id="membershipType"
-              name="membershipType"
-              value={formData.membershipType}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-lg"
-              required
-            >
-              <option value="">Typ av medlemskap</option>
-              <option value="familjemedlemskap">Familjemedlemskap</option>
-              <option value="stödmedlem">Stödmedlem</option>
-              <option value="enskild medlem">Enskild medlem</option>
-            </select>
+      <div className="flex justify-center items-center min-h-screen bg-page p-5 font-sans">
+        <div className="bg-white shadow-lg rounded-lg p-8 max-w-4xl w-full">
+          <h1 className="text-3xl font-bold mb-4 text-center">Bli medlem</h1>
+
+          <div className="flex">
+            <div className="w-1/2 pr-8">
+              <p className="mb-4 font-bold text-2xl">
+                Skicka din ansökan till oss idag och bli medlem i Arlanda MX.
+              </p>
+              <h2 className="text-2xl font-semibold mb-3">Medlemsavgifter</h2>
+              <ol>Enskild medlem 1200kr</ol>
+              <ol>Familjemedlem 1400kr</ol>
+              <ol>Stödmedlem 500kr</ol>
+              <p> Läs mer om de olika medlemskapen på
+                <a href="/members" className="text-blue-600 hover:underline "> Medlemmar</a>
+              </p>
+              <h2 className="text-2xl font-semibold mb-3 mt-4">Medlemsförmåner</h2>
+              <ol className="list-decimal list-inside">
+                <ol className="mb-4">Träna till lägre priser!</ol>
+                <ol  className="mb-4">Möjlighet att tjäna ihop till träningskort så att du får träna gratis på alla AMCs banor (läs mer
+                  under Medlemmar)
+                </ol>
+                <ol  className="mb-4" >Köpa snygga klubbprodukter till rabatterat pris </ol>
+                <ol  className="mb-4">Få 50 % tillbaka på nationella tävlingsavgifter som aktiv medlem och om du kör med AMCs klubbtröja
+                  och innehar träningskort för innevarande år.
+                </ol>
+                <ol  className="mb-4">Delta i det tränarledda klubbträningar gratis</ol>
+                <ol  className="mb-4">Delta i läger till subventionerade priser</ol>
+                <ol  className="mb-4">Vara med i årets mest prestigfyllda tävling, KM</ol>
+              </ol>
+            </div>
+            <div className="w-1/2">
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <label
+                      className="block text-gray-700 font-bold mb-2"
+                      htmlFor="membershipType"
+                  >
+                    Typ av medlemskap
+                  </label>
+                  <select
+                      id="membershipType"
+                      name="membershipType"
+                      value={formData.membershipType}
+                      onChange={handleChange}
+                      className="w-full px-3 py-2 border rounded-lg"
+                      required
+                  >
+                    <option value="">Typ av medlemskap</option>
+                    <option value="familjemedlemskap">Familjemedlemskap</option>
+                    <option value="stödmedlem">Stödmedlem</option>
+                    <option value="enskild medlem">Enskild medlem</option>
+                  </select>
+                </div>
+                <div className="mb-4">
+                  <label
+                      className="block text-gray-700 font-bold mb-2"
+                      htmlFor="firstName"
+                  >
+                    Förnamn
+                  </label>
+                  <input
+                      type="text"
+                      id="firstName"
+                      name="firstName"
+                      value={formData.firstName}
+                      onChange={handleChange}
+                      className="w-full px-3 py-2 border rounded-lg"
+                      required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                      className="block text-gray-700 font-bold mb-2"
+                      htmlFor="lastName"
+                  >
+                    Efternamn
+                  </label>
+                  <input
+                      type="text"
+                      id="lastName"
+                      name="lastName"
+                      value={formData.lastName}
+                      onChange={handleChange}
+                      className="w-full px-3 py-2 border rounded-lg"
+                      required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                      className="block text-gray-700 font-bold mb-2"
+                      htmlFor="personalNumber"
+                  >
+                    Fullständigt personnummer
+                  </label>
+                  <input
+                      type="text"
+                      id="personalNumber"
+                      name="personalNumber"
+                      value={formData.personalNumber}
+                      onChange={handleChange}
+                      className="w-full px-3 py-2 border rounded-lg"
+                      required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                      className="block text-gray-700 font-bold mb-2"
+                      htmlFor="email"
+                  >
+                    E-post
+                  </label>
+                  <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                      className="w-full px-3 py-2 border rounded-lg"
+                      required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                      className="block text-gray-700 font-bold mb-2"
+                      htmlFor="phone"
+                  >
+                    Telefonnummer
+                  </label>
+                  <input
+                      type="tel"
+                      id="phone"
+                      name="phone"
+                      value={formData.phone}
+                      onChange={handleChange}
+                      className="w-full px-3 py-2 border rounded-lg"
+                      required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label
+                      className="block text-gray-700 font-bold mb-2"
+                      htmlFor="sport"
+                  >
+                    Sportgren
+                  </label>
+                  <select
+                      id="sport"
+                      name="sport"
+                      value={formData.sport}
+                      onChange={handleChange}
+                      className="w-full px-3 py-2 border rounded-lg"
+                      required
+                  >
+                    <option value="">Välj sportgren</option>
+                    <option value="enduro">Enduro</option>
+                    <option value="motocross">Motocross</option>
+                  </select>
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 font-bold mb-2">
+                    <input
+                        type="checkbox"
+                        name="consent"
+                        checked={formData.consent}
+                        onChange={handleChange}
+                        className="mr-2 leading-tight"
+                        required
+                    />
+                    Jag ger mitt medgivande till att denna hemsida sparar
+                    informationen i detta formulär i syfte att kunna kontakta mig.
+                  </label>
+                </div>
+                <div className="mb-4">
+                  <label
+                      className="block text-gray-700 font-bold mb-2"
+                      htmlFor="verification"
+                  >
+                    Skriv följande siffror i fältet (43439)
+                  </label>
+                  <input
+                      type="text"
+                      id="verification"
+                      name="verification"
+                      value={formData.verification}
+                      onChange={handleChange}
+                      className="w-full px-3 py-2 border rounded-lg"
+                      required
+                  />
+                </div>
+                <button
+                    type="submit"
+                    className="w-full py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition duration-300 transform hover:scale-105"
+                >
+                  Skicka ansökan
+                </button>
+              </form>
+            </div>
           </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-bold mb-2"
-              htmlFor="firstName"
-            >
-              Förnamn
-            </label>
-            <input
-              type="text"
-              id="firstName"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-lg"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-bold mb-2"
-              htmlFor="lastName"
-            >
-              Efternamn
-            </label>
-            <input
-              type="text"
-              id="lastName"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-lg"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-bold mb-2"
-              htmlFor="personalNumber"
-            >
-              Fullständigt personnummer
-            </label>
-            <input
-              type="text"
-              id="personalNumber"
-              name="personalNumber"
-              value={formData.personalNumber}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-lg"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-bold mb-2"
-              htmlFor="email"
-            >
-              E-post
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-lg"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-bold mb-2"
-              htmlFor="phone"
-            >
-              Telefonnummer
-            </label>
-            <input
-              type="tel"
-              id="phone"
-              name="phone"
-              value={formData.phone}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-lg"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-bold mb-2"
-              htmlFor="sport"
-            >
-              Sportgren
-            </label>
-            <input
-              type="text"
-              id="sport"
-              name="sport"
-              value={formData.sport}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-lg"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 font-bold mb-2">
-              <input
-                type="checkbox"
-                name="consent"
-                checked={formData.consent}
-                onChange={handleChange}
-                className="mr-2 leading-tight"
-                required
-              />
-              Jag ger mitt medgivande till att denna hemsida sparar
-              informationen i detta formulär i syfte att kunna kontakta mig.
-            </label>
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 font-bold mb-2"
-              htmlFor="verification"
-            >
-              Skriv följande siffror i fältet (43439)
-            </label>
-            <input
-              type="text"
-              id="verification"
-              name="verification"
-              value={formData.verification}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-lg"
-              required
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition duration-300"
-          >
-            Skicka ansökan
-          </button>
-        </form>
+        </div>
       </div>
-    </div>
   );
 };
 
