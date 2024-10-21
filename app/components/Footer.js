5d9297537cc7e10ecde58409f58f4973e3c1d1f1
MikaelaMathisson <128062820+MikaelaMathisson@users.noreply.github.com>
2024-10-21 17:44:21 +0200

    changes to footer, adding border

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,10 +5,10 @@ import Link from "next/link";
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 text-white py-8">
-            <div className="container mx-auto px-4 flex flex-wrap justify-between">
+        <footer className="bg-gray-800 text-white py-8 relative z-10">
+            <div className="container mx-auto px-4 flex flex-wrap justify-between items-center">
                 {/* Kontaktsektion */}
-                <div className="w-full md:w-1/3 mb-8 text-center md:text-left">
+                <div className="w-full md:w-1/3 mb-4 text-center md:text-left flex flex-col items-center md:items-start">
                     <h2 className="text-2xl font-bold mb-4">Kontakt</h2>
                     <p><a href="mailto:info@arlandamc.se" className="hover:text-gray-400">Email: info@arlandamc.se</a></p>
                     <p><a href="tel:0812345678" className="hover:text-gray-400">Telefon: 08-123 456 78</a></p>
@@ -21,7 +21,7 @@ const Footer = () => {
                 </div>
 
                 {/* Menysektion */}
-                <div className="w-full md:w-1/3 mb-8 text-center">
+                <div className="w-full md:w-1/3 mb-4 text-center flex flex-col items-center border-t border-gray-600 pt-4 md:pt-0 md:border-t-0 md:border-l md:border-gray-600">
                     <h2 className="text-2xl font-bold mb-4">Meny</h2>
                     <ul className="grid grid-cols-2 gap-2">
                         <li><a href="/about-us" className="hover:text-gray-400">Om oss</a></li>
@@ -34,8 +34,8 @@ const Footer = () => {
                 </div>
 
                 {/* Logosektion */}
-                <div className="w-full md:w-1/3 mb-8 text-center md:text-right">
-                    <Link href="/" className="text-logo whitespace-nowrap">
+                <div className="w-full md:w-1/3 mb-4 text-center md:text-right flex flex-col items-center md:items-end border-t border-gray-600 pt-4 md:pt-0 md:border-t-0 md:border-l md:border-gray-600">
+                    <Link href="/" className=" whitespace-nowrap text-2xl font-bold text-yellow-500 text-outline">
                         ARLANDA MC
                     </Link>
                     <p>Förening För Motocross Enduro</p>
