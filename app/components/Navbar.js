c26d542e20a6a1cfb646f936547f95cd5901bbd7
MikaelaMathisson <128062820+MikaelaMathisson@users.noreply.github.com>
2024-10-22 16:30:24 +0200

    adding dropdown menys and fixing problems

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -10,12 +10,32 @@ import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 const Navbar = () => {
     const currentPath = usePathname();
     const router = useRouter();
+    const [isMotocrossDropdownOpen, setIsMotocrossDropdownOpen] = useState(false);
+    const [isMembersDropdownOpen, setIsMembersDropdownOpen] = useState(false);
 
     const linkClasses = (path) =>
         `mr-10 hover:text-gray-700 font-bold ${currentPath === path ? 'text-yellow-500' : 'text-white'}`;
 
+    const handleNavigation = (path) => {
+        router.push(path);
+    };
+
     const handleMotocrossClick = () => {
-        router.push('/mx');
+        handleNavigation('/mx');
+    };
+
+    const handleMotocrossDropdownToggle = (e) => {
+        e.stopPropagation();
+        setIsMotocrossDropdownOpen(!isMotocrossDropdownOpen);
+    };
+
+    const handleMembersClick = () => {
+        handleNavigation('/members');
+    };
+
+    const handleMembersDropdownToggle = (e) => {
+        e.stopPropagation();
+        setIsMembersDropdownOpen(!isMembersDropdownOpen);
     };
 
     return (
@@ -29,24 +49,71 @@ const Navbar = () => {
                 <Link href="/latestNews" className={linkClasses('/latestNews')} title="Senaste nytt">
                     Senaste nytt
                 </Link>
-                <button onClick={handleMotocrossClick} className={`${linkClasses('/mx')} flex items-center`} title="MX">
-                    Motocross
-                    <FontAwesomeIcon icon={faChevronDown} className="ml-2" size="xs"/>
-                </button>
+                <div className="relative">
+                    <button onClick={handleMotocrossClick} className={`${linkClasses('/mx')} flex items-center`} title="MX">
+                        Motocross
+                        <FontAwesomeIcon icon={faChevronDown} className="ml-2" size="xs" onClick={handleMotocrossDropdownToggle}/>
+                    </button>
+                    {isMotocrossDropdownOpen && (
+                        <div className="absolute top-full left-0 mt-2 w-48 bg-white shadow-lg rounded-lg">
+                            <Link href="/mx/baninfo" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                Baninfo
+                            </Link>
+                            <Link href="/mx/prislista" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                Prislista
+                            </Link>
+                            <Link href="/mx/faq" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                Vanliga frågor
+                            </Link>
+                            <Link href="/mx/gobraap" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                GoBraap
+                            </Link>
+                            <Link href="/mx/licenser" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                Licenser & Försäkringar
+                            </Link>
+                        </div>
+                    )}
+                </div>
                 <Link href="/enduro" className={`${linkClasses('/enduro')} flex items-center`} title="Enduro">
                     Enduro
                     <FontAwesomeIcon icon={faChevronDown} className="ml-2" size="xs" />
                 </Link>
-                <Link href="/members" className={`${linkClasses('/members')} flex items-center`} title="Medlemmar">
-                    Medlemmar
-                    <FontAwesomeIcon icon={faChevronDown} className="ml-2" size="xs" />
-                </Link>
+                <div className="relative">
+                    <button onClick={handleMembersClick} className={`${linkClasses('/members')} flex items-center`} title="Medlemmar">
+                        Medlemmar
+                        <FontAwesomeIcon icon={faChevronDown} className="ml-2" size="xs" onClick={handleMembersDropdownToggle}/>
+                    </button>
+                    {isMembersDropdownOpen && (
+                        <div className="absolute top-full left-0 mt-2 w-48 bg-white shadow-lg rounded-lg">
+                            <Link href="/members/klubbinfo" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                Klubbinfo
+                            </Link>
+                            <Link href="/members/medlemsinfo" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                Medlemsinfo
+                            </Link>
+                            <Link href="/members/boka-kioskdag" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                Boka kioskdag
+                            </Link>
+                            <Link href="/members/instruktioner-kiosken" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                Instruktioner kiosken
+                            </Link>
+                            <Link href="/members/kiosk-kalender" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                Kiosk kalender & Ersättare
+                            </Link>
+                            <Link href="/members/utbildning-tavlingsfunktion" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                Utbildning tävlingsfunktion
+                            </Link>
+                            <Link href="/members/arlanda-rc" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                Arlanda RC
+                            </Link>
+                        </div>
+                    )}
+                </div>
                 <Link href="/member" className={linkClasses('/member')} title="Bli medlem">
                     Bli medlem
                 </Link>
                 <Link href="/crosskola" className={`${linkClasses('/crosskola')} flex items-center`} title="Crosskola">
                     Crosskola
-                    <FontAwesomeIcon icon={faChevronDown} className="ml-2" size="xs" />
                 </Link>
                 <Link href="/calendar" className={linkClasses('/calendar')} title="Kalender">
                     <FontAwesomeIcon icon={faCalendarAlt} size="lg" />
