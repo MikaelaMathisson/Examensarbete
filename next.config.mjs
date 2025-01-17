54726fb21ad365971e0fdace3110d4cd388848af
MikaelaMathisson <mikaela.mathisson@hotmail.se>
2025-01-17 11:51:14 +0100

    adding postgres database

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        DATABASE_URL: 'postgres://postgres:mysecretpassword@localhost:5432/postgres'
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: ['example.com'],
+    },
+};
 
 export default nextConfig;
